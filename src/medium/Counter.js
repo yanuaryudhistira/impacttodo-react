f898488e3174106c0447b8f81bac5ad22a3baa51
M Haidar Hanif <git@mhaidarhanif.com>
2018-04-23 15:24:27 +0700

    Implement counter increment & decrement

--- src/medium/Counter.js
+++ src/medium/Counter.js
@@ -1,20 +1,37 @@
 import React from "react";
+import "./Counter.css";
 
 export default class Counter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentValue: 0
+      value: 0
     };
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+
+  increment() {
+    this.setState(prevState => {
+      return { value: prevState.value + 1 };
+    });
+  }
+
+  decrement() {
+    this.setState(prevState => {
+      return { value: prevState.value - 1 };
+    });
   }
 
   render() {
-    const currentValue = this.state.currentValue;
+    const value = this.state.value;
 
     return (
       <div>
-        <h4>Counter Component</h4>
-        {currentValue}
+        <h3>Counter Component</h3>
+        <button onClick={this.decrement}>-</button>
+        <span>{value}</span>
+        <button onClick={this.increment}>+</button>
       </div>
     );
   }
